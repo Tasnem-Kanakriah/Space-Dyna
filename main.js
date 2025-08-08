8780fd968213c48c10ebd894d2cafa9bb594c758
tasnem kanakriah <tasnemkanakriah9302@gmail.com>
2025-08-08 23:47:18 +0300

    first_commit_nav_bar

---
+++ main.js
@@ -0,0 +1,6 @@
+// menu bar for nav bar
+const menuBar = document.getElementById('menu-bar');
+menuBar.addEventListener('click', function () {
+    this.classList.toggle('open')
+})
+// // menu bar for nav bar
