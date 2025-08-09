697cea1e4bac9c9335d6299fc113dc54c1eaea44
tasnem kanakriah <tasnemkanakriah9302@gmail.com>
2025-08-09 18:29:59 +0300

    seconde_commit_header

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ menuBar.addEventListener('click', function () {
     this.classList.toggle('open')
 })
 // // menu bar for nav bar
+
+// sticky navbar after 200px
+window.onscroll = function () {
+    if (window.scrollY > 226) {
+        document.getElementById('navbar').style.position = 'sticky';
+        document.getElementById('navbar').style.top = 0;
+        // console.log('greater than 200px');
+    }
+    else {
+        document.getElementById('navbar').style.position = 'static';
+    }
+}
+// // sticky navbar after 200px
